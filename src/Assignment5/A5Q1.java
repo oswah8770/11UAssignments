9f4bc5657aa55860e4a8f14b37ab3738c6673dc8
oswah8770 <oswah8770@HRH-IC0027337.wrdsb.ca>
2017-10-26 13:10:47 -0400

    Finished Assignment 4 Question 10 & Quiz 1

---
+++ src/Assignment5/A5Q1.java
@@ -0,0 +1,62 @@
+package Assignment5;
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * translate word inputted into ubbi dubbi
+ *
+ * @author Hunter Oswald
+ */
+public class A5Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // ask for a word and get input for it
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a word:");
+        String word = input.nextLine();
+
+        // convert to lowercase
+        word = word.toLowerCase();
+
+
+
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i' || word.charAt(i) == 'o' || word.charAt(i) == 'u') {
+                // when finds a vowel inserts ub before it. skip the next letter by adding 3. 1 for the next char, 2 for the ub added
+                String front = word.substring(0, i);
+                String ending = word.substring(i);
+
+                word = front + "ub" + ending;
+                
+                i = i + 3;
+                
+                
+                
+                
+                
+                
+               
+                    
+
+
+
+            }
+
+
+
+        }
+        // output translated word
+        System.out.println("In Ubbi Dubbi is: " + word);
+
+
+
+    }
+}
