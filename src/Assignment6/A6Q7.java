7af39541754645def3562c2ee662b1ae196f8f34
oswah8770 <oswah8770@HRH-IC0027337.wrdsb.ca>
2017-11-20 12:56:58 -0500

    Finished Assignment 6

--- src/Assignment6/A6Q7.java
+++ src/Assignment6/A6Q7.java
@@ -4,6 +4,8 @@
  */
 package Assignment6;
 
+import java.util.Scanner;
+
 /**
  * Make a program that finds all the prime numbers between 2 - 1000 using the
  * sieve of Eratosthenes
@@ -30,20 +32,19 @@ public class A6Q7 {
             numArray[i] = i;
         }
 
-        // for loop to mark everything as 2, undefined. 1 is prime 0 is not prime.
+        // for loop to mark everything as true
         for (int u = 0; u < numArray.length; u++) {
             primeOrNah[u] = true;
         }
         
         primeOrNah[2] = true;
-        // for loop to mark things as not prime
+        // for loop to mark things as not prime or prime
         for (int i = 2; i < numArray.length; i++) {
             // for loop define all non prime
             for (int z = t * 2; z < numArray.length; z = z + t) {
                 primeOrNah[z] = false;
                 }
-            // find next non marked number (true in this case is not marked, until the entire process is finished where it is then prime)
-            int store = t;
+            // find next non marked number (true in this case is not marked, until the entire process is finished where it is then prime. t is then this prime number)
             for (int x = t; x < numArray.length; x = x + 1) {
                 if (t < primeOrNah.length - 1) {    
                 t++;
@@ -57,9 +58,16 @@ public class A6Q7 {
             }
         
         // output
-        int yo = 2;
-        System.out.println(numArray[yo] + ", " + primeOrNah[yo]);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a number");
+        int numInput = input.nextInt();
         
+        if (primeOrNah[numInput] == true) {
+        
+        System.out.println(numArray[numInput] + ", is a prime number");
+        } else {
+            System.out.println(numArray[numInput] + " is NOT a prime number");
+        }
         
     
     
