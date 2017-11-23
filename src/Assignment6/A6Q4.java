e1a206b037e903a08f7b1b26d9601af84b3c6ad7
oswah8770 <oswah8770@HRH-IC0027337.wrdsb.ca>
2017-11-23 14:25:11 -0500

    Finished Assignment 6

--- src/Assignment6/A6Q4.java
+++ src/Assignment6/A6Q4.java
@@ -35,7 +35,7 @@ public class A6Q4 {
         double store = 0;
         // sorting
         for(int z = 0; z < marks.length; z++) {
-            // execute this second for loop the same amount of times as the length - 1 because don't want to compare position 10 to 11 (11 doesn't exist)
+            // execute this second for loop the same amount of times as the length - 1 because don't want to compare position 9 to 10 (10 doesn't exist)
             for (int y = 0; y < marks.length - 1; y++) {
                 if (marks[y] > marks[y+1]) {
                     store = marks[y];
@@ -52,8 +52,11 @@ public class A6Q4 {
         // output the marks in ascending order
         System.out.println("The marks in ascending order are: ");
         for (int x = 0; x < marks.length; x++) {
+            if (marks[x] != marks.length) {
             System.out.print(marks[x] + ", ");
-            
+            } else {
+                System.out.println(marks[x]);
+            }
             
             
             
